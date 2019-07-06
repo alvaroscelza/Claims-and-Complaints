2d7e897302cde6e5c5d26316b44f50e346fc00a6
Alvaro Andrés Rodríguez Scelza <Alvaroscelza@gmail.com>
2019-07-05 21:53:31 -0300

    index and root page is done

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root 'static_pages#index'
+  get '/static_pages/index'
+
   resources :judgements
   resources :users
   resources :companies
   resources :businesses
   resources :permits
   resources :roles
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
