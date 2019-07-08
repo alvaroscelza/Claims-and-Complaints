f9464c0197c54a537e365701698d5edca07e84c8
Alvaro Andrés Rodríguez Scelza <Alvaroscelza@gmail.com>
2019-07-07 22:54:59 -0300

    added seed users and improved seed roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,19 @@
-Role.create!([{ name: 'administrator' }, { name: 'user' }])
+# Seeds for any environment
+
+administrator_role = Role.create!({ name: 'administrator' })
+common_user_role = Role.create!({ name: 'user' })
+
+User.create!({ email: 'alvaroscelza@gmail.com',
+               password: '123456',
+               role: administrator_role,
+               confirmed_at: '2019-07-08 00:00:00.000' })
+
+if Rails.env.production?
+end
+
+if Rails.env.development?
+  User.create!({ email: 'user@gmail.com',
+                 password: '123456',
+                 role: common_user_role,
+                 confirmed_at: '2019-07-08 00:00:00.000'})
+end
