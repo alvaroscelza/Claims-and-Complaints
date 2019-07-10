4c52515271a7169e32cd1f6847ef62b4e4ff6eb7
Alvaro Andrés Rodríguez Scelza <Alvaroscelza@gmail.com>
2019-07-09 22:14:53 -0300

    updated helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,2 @@
 module UsersHelper
-  def user_is_admin?
-    @user_role = current_user.role
-    user_attributes = current_user.attributes
-    user_role_id = user_attributes.role
-    role = Role.find_by(id: user_role_id)
-  end
 end
