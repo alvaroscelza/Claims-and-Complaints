5c21fb69ef3d64f2c3ee64d0057ef46773735b0d
Alvaro Andrés Rodríguez Scelza <Alvaroscelza@gmail.com>
2019-07-10 17:45:56 -0300

    experimenting with routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get '/static_pages/index'
 
   devise_for :users
-  resources :users
+  get '/users', to: 'users#index', as: :users
+  get '/users/:id', to: 'users#show', as: :user
+  get '/users/new', to: 'users#new', as: :new_user
+  get '/users/edit', to: 'users#edit', as: :edit_user
+  post '/users', to: 'users#create', as: :create_user
   resources :judgements
   resources :companies
   resources :businesses
