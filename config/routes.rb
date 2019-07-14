f807148d0482cfeb959c85431124347eed8c0749
Alvaro Andrés Rodríguez Scelza <Alvaroscelza@gmail.com>
2019-07-13 22:15:03 -0300

    removed user views, controller actions and routes that will not be used.

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   get '/users', to: 'users#index', as: :users
-  get '/users/:id', to: 'users#show', as: :user
-  get '/users/new', to: 'users#new', as: :new_user
-  get '/users/edit', to: 'users#edit', as: :edit_user
-  post '/users', to: 'users#create', as: :create_user
+  # get '/users/:id', to: 'users#show', as: :user
+  # get '/users/new', to: 'users#new', as: :new_user
+  # get '/users/edit', to: 'users#edit', as: :edit_user
+  # post '/users', to: 'users#create', as: :create_user
   resources :judgements
   resources :companies
   resources :businesses
