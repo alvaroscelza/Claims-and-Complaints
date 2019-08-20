e6c7a00e0611abbce754b0512a63cb41ac9d72fc
Alvaro Andrés Rodríguez Scelza <Alvaroscelza@gmail.com>
2019-08-19 21:25:40 -0300

    refactor de update admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,23 +6,22 @@ class UsersController < ApplicationController
   end
 
   def update_administrators
-    begin
-      new_admins_emails = user_params
-      User.find_each do |user|
-        if new_admins_emails.include?(user.email)
-          user.update(is_admin: true)
-        else
-          user.update(is_admin: false)
-        end
+    new_admins_emails = user_params
+    User.find_each do |user|
+      if new_admins_emails.include?(user.email)
+        user.update(is_admin: true)
+      else
+        user.update(is_admin: false)
       end
-      respond_to do |format|
-        format.html { redirect_to users_path, notice: 'Users were successfully updated.' }
-        format.json { render users_path, status: :ok }
-      end
-    rescue Exception => ex
-      format.html { redirect_to users_path, notice: ex.message }
-      format.json { render json: ex.message, status: :unprocessable_entity }
     end
+    respond_to do |format|
+      format.html { redirect_to users_path, notice: 'Users were successfully updated.' }
+      format.json { render users_path, status: :ok }
+    end
+  rescue Exception => ex
+    format.html { redirect_to users_path, notice: ex.message }
+    format.json { render json: ex.message, status: :unprocessable_entity }
+
   end
 
   private
