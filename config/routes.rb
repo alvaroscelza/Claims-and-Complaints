c7e7624fed9fb085da5b5f9f690075a9af77e8c2
Alvaro Andrés Rodríguez Scelza <Alvaroscelza@gmail.com>
2019-08-19 21:49:31 -0300

    nested route for judgement inside company

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   get '/users', to: 'users#index', as: :users
   patch '/users/update_administrators', to: 'users#update_administrators', as: :update_administrators
-  resources :judgements
-  resources :companies
+  resources :companies do
+    resources :judgements
+  end
   resources :businesses
 end
