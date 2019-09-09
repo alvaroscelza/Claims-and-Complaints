127d4a0eff71ffc3241a8581bbd6fa25f71ff189
Ing. Alvaro Rodríguez Scelza <alvaroscelza@gmail.com>
2019-09-08 23:08:52 -0300

    Se eliminan imagenes de prueba.
    se popula seeds con compa;ia y rubro de prueba para development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,22 @@
+# frozen_string_literal: true
+
 # Seeds for any environment
-User.create!({ email: 'alvaroscelza@gmail.com',
-               password: '123456',
-               is_admin: true,
-               confirmed_at: '2019-07-08 00:00:00.000' })
+User.create!(email: 'alvaroscelza@gmail.com',
+             password: '123456',
+             is_admin: true,
+             confirmed_at: '2019-07-08 00:00:00.000')
 
 if Rails.env.production?
 end
 
 if Rails.env.development?
-  User.create!({ email: 'user@gmail.com',
-                 password: '123456',
-                 is_admin: false,
-                 confirmed_at: '2019-07-08 00:00:00.000'})
+  User.create!(email: 'user@gmail.com',
+               password: '123456',
+               is_admin: false,
+               confirmed_at: '2019-07-08 00:00:00.000')
+  Business.create!(id: 1,
+                   name: 'Restaurante')
+  Company.create!(name: "McDonald's",
+                  reputation: 0,
+                  business_id: 1)
 end
