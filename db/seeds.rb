66e55459d43692ad7e4d4dcacbef27f414a31e5c
Ing. Alvaro Rodríguez Scelza <alvaroscelza@gmail.com>
2019-09-11 00:12:10 -0300

    seeds para juicio de prueba sobre mc donalds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 # Seeds for any environment
-User.create!(email: 'alvaroscelza@gmail.com',
+User.create!(id: 1,
+             email: 'alvaroscelza@gmail.com',
              password: '123456',
              is_admin: true,
              confirmed_at: '2019-07-08 00:00:00.000')
@@ -10,13 +11,18 @@ if Rails.env.production?
 end
 
 if Rails.env.development?
-  User.create!(email: 'user@gmail.com',
+  User.create!(id: 2,
+               email: 'user@gmail.com',
                password: '123456',
                is_admin: false,
                confirmed_at: '2019-07-08 00:00:00.000')
   Business.create!(id: 1,
                    name: 'Restaurante')
-  Company.create!(name: "McDonald's",
+  Company.create!(id: 1,
+                  name: "McDonald's",
                   reputation: 0,
                   business_id: 1)
+  Judgement.create!(opinion: 'Opinion de prueba',
+                    user_id: 2,
+                    company_id: 1)
 end
