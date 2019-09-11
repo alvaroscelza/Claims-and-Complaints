8be287914f63fe3100edd719a0d0efd73c61e81a
Ing. Alvaro Rodríguez Scelza <alvaroscelza@gmail.com>
2019-09-11 00:24:26 -0300

    deleted judgements unused endpoints

--- app/controllers/judgements_controller.rb
+++ app/controllers/judgements_controller.rb
@@ -1,22 +1,12 @@
 class JudgementsController < ApplicationController
-  before_action :set_judgement, only: %i[show edit update destroy]
-
-  def index
-    @judgements = Judgement.all
-    respond_with(@judgements)
-  end
-
-  def show
-    respond_with(@judgement)
-  end
+  before_action :set_judgement, only: %i[edit update destroy]
 
   def new
     @judgement = Judgement.new
     respond_with(@judgement)
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @judgement = Judgement.new(judgement_params)
