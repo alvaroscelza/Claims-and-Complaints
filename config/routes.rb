9e8caf67853c570e41267181dbcc416ce471843f
alvaroscelza@gmail.com <8cB8EqmEwna45aG>
2020-10-10 17:44:59 -0300

    corrected users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
+  get '/users', to: 'users#index'
+  patch '/users', to: 'users#update_administrators'
   resources :judgements
   resources :companies
   resources :businesses
