e4c16a2e5d415523a477ab31b1164fe66aa93092
alvaroscelza@gmail.com <8cB8EqmEwna45aG>
2020-11-14 19:45:14 -0300

    corrected page title

---
+++ api/helpers/page-title.js
@@ -0,0 +1,16 @@
+module.exports = {
+  friendlyName: 'Page title',
+  description: 'Infers page title by concatenating app name with current page name.',
+  sync: true,
+
+  inputs: {
+    page_name: {
+      type: 'string'
+    }
+  },
+
+  fn: function ({ page_name }) {
+    base_title = 'Claims and Complaints'
+    return Boolean(page_name) ? `${page_name} | ${base_title}` : base_title
+  }
+};
