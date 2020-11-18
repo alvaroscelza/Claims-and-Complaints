e01737533379c0ce735deeb1857bd3fc1a1de9ba
alvaroscelza@gmail.com <8cB8EqmEwna45aG>
2020-11-18 00:04:37 -0300

    deleted email code

--- config/custom.js
+++ config/custom.js
@@ -3,31 +3,4 @@ module.exports.custom = {
   passwordResetTokenTTL: 24*60*60*1000,// 24 hours
   emailProofTokenTTL:    24*60*60*1000,// 24 hours
   rememberMeCookieMaxAge: 30*24*60*60*1000, // 30 days
-
-  /**************************************************************************
-  *                                                                         *
-  * Automated email configuration                                           *
-  *                                                                         *
-  * Sandbox Sendgrid credentials for use during development, as well as any *
-  * other default settings related to "how" and "where" automated emails    *
-  * are sent.                                                               *
-  *                                                                         *
-  * (https://app.sendgrid.com/settings/api_keys)                            *
-  *                                                                         *
-  **************************************************************************/
-  // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
-  //--------------------------------------------------------------------------
-  // /\  Configure this to enable support for automated emails.
-  // ||  (Important for password recovery, verification, contact form, etc.)
-  //--------------------------------------------------------------------------
-
-  // The sender that all outgoing emails will appear to come from.
-  fromEmailAddress: 'noreply@example.com',
-  fromName: 'The Claims and Complaints Team',
-
-  // Email address for receiving support messages & other correspondences.
-  // > If you're using the default privacy policy, this will be referenced
-  // > as the contact email of your "data protection officer" for the purpose
-  // > of compliance with regulations such as GDPR.
-  internalEmailAddress: 'support+development@example.com',
 };
