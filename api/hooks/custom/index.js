8e872302388c796d4c95f76d7ad411a47328570f
alvaroscelza@gmail.com <8cB8EqmEwna45aG>
2020-11-18 00:04:37 -0300

    deleted email code

--- api/hooks/custom/index.js
+++ api/hooks/custom/index.js
@@ -7,71 +7,9 @@ module.exports = function defineCustomHook(sails) {
     /**
      * Runs when a Sails app loads/lifts.
      */
-    initialize: async function () {
-
-      sails.log.info('Initializing project hook... (`api/hooks/custom/`)');
-
-      var IMPORTANT_SENDGRID_CONFIG = ['sendgridSecret', 'internalEmailAddress'];
-      var isMissingSendgridConfig = _.difference(IMPORTANT_SENDGRID_CONFIG, Object.keys(sails.config.custom)).length > 0;
-
-      if (isMissingSendgridConfig) {
-        let suffix = '';
-        if (_.contains(['silly'], sails.config.log.level)) {
-          suffix =
-`
-> Tip: To exclude sensitive credentials from source control, use:
-> • config/local.js (for local development)
-> • environment variables (for production)
->
-> If you want to check them in to source control, use:
-> • config/custom.js  (for development)
-> • config/env/staging.js  (for staging)
-> • config/env/production.js  (for production)
->
-> (See https://sailsjs.com/docs/concepts/configuration for help configuring Sails.)
-`;
-        }
-
-        let problems = [];
-        if (sails.config.custom.sendgridSecret === undefined) {
-          problems.push('No `sails.config.custom.sendgridSecret` was configured.');
-        }
-        if (sails.config.custom.internalEmailAddress === undefined) {
-          problems.push('No `sails.config.custom.internalEmailAddress` was configured.');
-        }
-
-        sails.log.verbose(
-`Some optional settings have not been configured yet:
----------------------------------------------------------------------
-${problems.join('\n')}
-
-Until this is addressed, this app's ${isMissingSendgridConfig} features
-will be disabled and/or hidden in the UI.
-
- [?] If you're unsure or need advice, come by https://sailsjs.com/support
----------------------------------------------------------------------${suffix}`);
-      }//ﬁ
-
-      // After "sails-hook-organics" finishes initializing, configure 
-      // and Sendgrid packs with any available credentials.
-      sails.after('hook:organics:loaded', ()=>{
-
-        sails.helpers.sendgrid.configure({
-          secret: sails.config.custom.sendgridSecret,
-          from: sails.config.custom.fromEmailAddress,
-          fromName: sails.config.custom.fromName,
-        });
-
-      });//_∏_
-
-      // ... Any other app-specific setup code that needs to run on lift,
-      // even in production, goes here ...
-
-    },
-
+    initialize: async function () {},
 
     routes: {
-
       /**
        * Runs before every matching route.
        *
@@ -150,12 +88,6 @@ will be disabled and/or hidden in the UI.
               return res.unauthorized();
             }
 
-            // Add additional information for convenience when building top-level navigation.
-            // (i.e. whether to display "Dashboard", "My Account", etc.)
-            if (!loggedInUser.password || loggedInUser.emailStatus === 'unconfirmed') {
-              loggedInUser.dontDisplayAccountLinkInNav = true;
-            }
-
             // Expose the user record as an extra property on the request object (`req.me`).
             // > Note that we make sure `req.me` doesn't already exist first.
             if (req.me !== undefined) {
@@ -214,14 +146,10 @@ will be disabled and/or hidden in the UI.
             // > • https://mixmax.com/blog/chrome-back-button-cache-no-store
             // > • https://madhatted.com/2013/6/16/you-do-not-understand-browser-history
             res.setHeader('Cache-Control', 'no-cache, no-store');
-
             return next();
           }
         }
       }
     }
-
-
   };
-
 };
