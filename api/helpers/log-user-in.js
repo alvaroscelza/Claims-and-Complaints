5f841cc0c71e0eac72bb8e3c5fcb68ebf5d83acc
alvaroscelza@gmail.com <8cB8EqmEwna45aG>
2020-11-24 06:21:23 -0300

    encapsulated log in method in a helper

---
+++ api/helpers/log-user-in.js
@@ -0,0 +1,16 @@
+module.exports = {
+  inputs: {
+    user: {
+      type: 'json',
+      required: true,
+    },
+    session: {
+      type: 'json',
+      required: true,
+    }
+  },
+
+  fn: async function ({ user, session }) {
+    session.userId = user.id;
+  }
+};
