736147a46527b9b99e778fca5043d7e5d8353148
alvaroscelza@gmail.com <8cB8EqmEwna45aG>
2020-11-24 07:11:02 -0300

    deleted unused code

--- api/controllers/entrance/signup.js
+++ api/controllers/entrance/signup.js
@@ -17,19 +17,9 @@ module.exports = {
   },
 
   exits: {
-    success: {
-      description: 'New user account was created successfully.'
-    },
-    invalid: {
-      responseType: 'badRequest',
-      description: 'The provided fullName, password and/or email address are invalid.',
-      extendedDescription: `If this request was sent from a graphical user interface, the request parameters should 
-      have been validated/coerced _before_ they were sent.`
-    },
-    emailAlreadyInUse: {
-      statusCode: 409,
-      description: 'The provided email address is already in use.',
-    },
+    success: {},
+    invalid: { responseType: 'badRequest' },
+    emailAlreadyInUse: { statusCode: 409, },
   },
 
   fn: async function ({email, password, fullName}) {
