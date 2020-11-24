18246f56c083a38a5a1c33b1f410d6b97729f257
alvaroscelza@gmail.com <8cB8EqmEwna45aG>
2020-11-24 07:12:53 -0300

    refactor code

--- api/controllers/entrance/view-signup.js
+++ api/controllers/entrance/view-signup.js
@@ -1,20 +1,11 @@
 module.exports = {
   exits: {
-    success: {
-      viewTemplatePath: 'pages/entrance/signup',
-    },
-    redirect: {
-      description: 'The requesting user is already logged in.',
-      responseType: 'redirect'
-    }
+    success: { viewTemplatePath: 'pages/entrance/signup', },
+    redirect: { responseType: 'redirect' }
   },
 
   fn: async function () {
-    if (this.req.me) {
-      throw {redirect: '/'};
-    }
-    return {
-      page_name: 'Sign up'
-    };
+    if (this.req.me) throw {redirect: '/'};
+    return { page_name: 'Sign up' };
   }
 };
