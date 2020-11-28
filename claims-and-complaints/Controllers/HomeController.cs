f118d9e46cf0700b31fe36e95bd4dc141a4fb2d8
alvaroscelza@gmail.com <8cB8EqmEwna45aG>
2020-11-28 20:53:44 -0300

    deleted privacy

--- claims-and-complaints/Controllers/HomeController.cs
+++ claims-and-complaints/Controllers/HomeController.cs
@@ -21,11 +21,6 @@ namespace claims_and_complaints.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
