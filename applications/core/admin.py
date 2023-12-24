f5af9ce013a04ee67f76d9bd97d424a4c9a5a310
Alvaro Scelza <alvaroscelza@gmail.com>
2023-12-24 19:23:37 -0300

    finished models V1

---
+++ applications/core/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+
+from applications.core.models import Business, Company, UserScoreModifierType, UserScoreModifierInstance, Judgement
+
+
+@admin.register(Business)
+class BusinessAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(UserScoreModifierType)
+class UserScoreModifierTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(UserScoreModifierInstance)
+class UserScoreModifierInstanceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Judgement)
+class JudgementAdmin(admin.ModelAdmin):
+    pass
