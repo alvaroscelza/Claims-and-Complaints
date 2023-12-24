e05216c811cadff1bf7a85da1da1664b83f1eb6c
Alvaro Scelza <alvaroscelza@gmail.com>
2023-12-24 19:23:37 -0300

    finished models V1

--- applications/core/models/companies.py
+++ applications/core/models/companies.py
@@ -1,14 +1,25 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 from applications.utils import UniqueNameMixin
 
 
 class Business(UniqueNameMixin):
-    pass
+    class Meta(UniqueNameMixin.Meta):
+        verbose_name = _('business')
+        verbose_name_plural = _('businesses')
 
 
 class Company(UniqueNameMixin):
+    class Meta(UniqueNameMixin.Meta):
+        verbose_name = _('company')
+        verbose_name_plural = _('companies')
+
     business = models.ForeignKey(Business, models.PROTECT)
-    score = models.IntegerField(default=0)
     logo = models.ImageField(upload_to='company/', blank=True, null=True)
     description = models.TextField(blank=True, null=True)
+
+    @property
+    def score(self) -> int:
+        # noinspection PyUnresolvedReferences
+        return sum(j.vote for j in self.judgement_set.all()) or 0
--- applications/core/models/judgements.py
+++ applications/core/models/judgements.py
@@ -4,8 +4,16 @@ from applications.core.models.companies import Company
 from applications.core.models.users import ClientUser
 
 
+class Votes(models.IntegerChoices):
+    VERY_BAD = -2
+    BAD = -1
+    NEUTRAL = 0
+    GOOD = 1
+    VERY_GOOD = 2
+
+
 class Judgement(models.Model):
     opinion = models.TextField()
-    vote = models.BooleanField()
+    vote = models.IntegerField(choices=Votes.choices)
     author = models.ForeignKey(ClientUser, models.CASCADE)
     company = models.ForeignKey(Company, models.CASCADE)
