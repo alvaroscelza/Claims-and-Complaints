81083500b8988082eb6572b73bb8326a03e33378
Alvaro Scelza <alvaroscelza@gmail.com>
2024-01-02 20:27:18 -0300

    added companies list

---
+++ applications/core/controllers/companies_controller.py
@@ -0,0 +1,13 @@
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+
+from applications.core.serializers.companies_serializer import CompanySerializer
+
+
+class CompaniesController(GenericViewSet):
+    serializer_class = CompanySerializer
+    queryset = serializer_class.Meta.model.objects.all()
+
+    def list(self, _):
+        companies = self.get_queryset()
+        return Response({'companies': companies}, template_name='companies/list.html')
--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
 
 # region Admin configuration
 urlpatterns = i18n_patterns(path('admin/', admin.site.urls), prefix_default_language=False)
@@ -13,8 +12,8 @@ admin.site.site_title = settings.APP_NAME
 # endregion
 
 # # region Core configuration
-# core_path = path('api/v1/core/', include('applications.core.urls'))
-# urlpatterns += i18n_patterns(core_path, prefix_default_language=False)
+core_path = path('', include('applications.core.urls'))
+urlpatterns += i18n_patterns(core_path, prefix_default_language=False)
 # # endregion
 
 # region Media configuration
@@ -22,6 +21,9 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # endregion
 
-# region Templates configuration
-urlpatterns += i18n_patterns(path('', TemplateView.as_view(template_name='index.html')), prefix_default_language=False)
+# region Generic Templates configuration
+# contact_template = TemplateView.as_view(template_name='contact.html')
+# urlpatterns += i18n_patterns(path(r'contact/', contact_template), prefix_default_language=False)
+# terms_and_conditions_template = TemplateView.as_view(template_name='terms_and_conditions.html')
+# urlpatterns += i18n_patterns(path(r'terms/', terms_and_conditions_template), prefix_default_language=False)
 # endregion
