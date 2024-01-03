0cfe5e10e4d12390b89d5b00f0cc5956970b2f7b
Abdullah Imran <abdullah.adam2002@gmail.com>
2024-01-03 18:15:16 +0000

    Add Base Urls and views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+
 
 # Create your views here.
+def profile(request):
+    pass
+
+
+def register(request):
+    pass
+
+
+def login(request):
+    if request.method == "POST":
+        raise Exception("Not configured")
+    form = None
+    context = {"form": form}
+    return render(request, "users/login.html", context)
+
+
+def logout(request):
+    logout(request)
+    return redirect("home")
