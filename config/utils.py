0b4ba082f492c1c02bd6ee034e182fdbbe0d7866
Abdullah Imran <abdullah.adam2002@gmail.com>
2024-01-07 20:29:59 +0000

    Fix function

--- config/utils.py
+++ config/utils.py
@@ -1,13 +1,29 @@
 from django.core.mail import send_mail
 from django.conf import settings
 from django.utils.html import strip_tags
+from django.template.loader import render_to_string
 
 
-def send_html_email(subject, html, plaintext, recipient, recipients=[]):
+def send_html_email(
+    subject,
+    html_template,
+    context,
+    recipient,
+    recipients=[],
+    request=None,
+    plaintext=None,
+):
     # Set Recipient List
     recipient_list = recipients + [recipient] if recipient else []
     if not recipient_list:
         raise Exception("No Recipients found")
+    # Base context values set incase of no request
+    html_context = {"brand_name": settings.BRAND_NAME, "absolute_domain": "", **context}
+    html = (
+        render_to_string(html_template, request=request, context=html_context)
+        if html_template
+        else None
+    )
     # Send Email
     send_mail(
         subject,
