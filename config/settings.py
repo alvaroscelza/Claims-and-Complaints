26d9c49b5745f83a372aa7917340eb17de8e50a0
Abdullah Imran <abdullah.adam2002@gmail.com>
2024-01-07 17:29:55 +0000

    Add gmail smtp settings

--- config/settings.py
+++ config/settings.py
@@ -147,3 +147,12 @@ LOGIN_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/"
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+# Email Variables
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD  # Gmail App Password
+EMAIL_FROM_NAME = "Claims & Complaints"  # The name users will see in their inbox
