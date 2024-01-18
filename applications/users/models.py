eabf856cb5507fbb8b80f0718cf677c082c889f8
Alvaro Scelza <alvaroscelza@gmail.com>
2024-01-18 15:39:07 -0300

    removed old users file, minor fixes in new one

--- applications/users/models.py
+++ applications/users/models.py
@@ -7,10 +7,8 @@ from config.utils import send_html_email
 from .token import account_activation_token, account_password_reset_token
 
 
-# User model inherits from Django's Existing model to retain built in functionality
 class User(AbstractUser):
-    # User Profile Attributes
-    profile_picture = models.ImageField(upload_to="user/", blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='user/', blank=True, null=True)
 
     # User Login/Signup Attributes
     email_validated = models.DateTimeField(blank=True, null=True, db_index=True)
@@ -18,24 +16,19 @@ class User(AbstractUser):
         blank=True,
         null=True,
         db_index=True,
-        help_text="Date user email was marked as invalid",
+        help_text='Date user email was marked as invalid',
     )
     # User Tracking
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        app_label = "users"
+        app_label = 'users'
 
     @property
     def score(self) -> int:
         # noinspection PyUnresolvedReferences
         score_modifiers = self.user_score_modifier_instance_set
-        return (
-            score_modifiers.aggregate(models.Sum("modifier_type__score"))[
-                "modifier_type__score__sum"
-            ]
-            or 0
-        )
+        return score_modifiers.aggregate(models.Sum('modifier_type__score'))['modifier_type__score__sum'] or 0
 
     @property
     def profile_picture_filename(self):
@@ -46,21 +39,21 @@ class User(AbstractUser):
     def get_activate_url(self):
         token = account_activation_token.make_token(self)
         return reverse(
-            "users:verify_email", kwargs={"user_id": self.id, "token": token}
+            'users:verify_email', kwargs={'user_id': self.id, 'token': token}
         )
 
     def get_password_reset_url(self):
         token = account_password_reset_token.make_token(self)
         return reverse(
-            "users:forgot_password", kwargs={"token": token, "user_id": self.id}
+            'users:forgot_password', kwargs={'token': token, 'user_id': self.id}
         )
 
     def send_verification_email(self, request=None):
         url = self.get_activate_url()
         send_html_email(
-            html_template="users/emails/register_verification.html",
-            context={"url": url, "user": self},
-            subject="Verify your Account",
+            html_template='users/emails/register_verification.html',
+            context={'url': url, 'user': self},
+            subject='Verify your Account',
             recipient=self.email,
             request=request,
         )
@@ -68,9 +61,9 @@ class User(AbstractUser):
     def send_forgot_password_email(self, request=None):
         url = self.get_password_reset_url()
         send_html_email(
-            html_template="users/emails/forgot_password.html",
-            context={"url": url, "user": self},
-            subject="Verify your Account",
+            html_template='users/emails/forgot_password.html',
+            context={'url': url, 'user': self},
+            subject='Verify your Account',
             recipient=self.email,
             request=request,
         )
@@ -81,6 +74,6 @@ class UserScoreModifierType(UniqueNameMixin):
 
 
 class UserScoreModifierInstance(models.Model):
-    user = models.ForeignKey("User", models.CASCADE)
+    user = models.ForeignKey(User, models.CASCADE)
     modifier_type = models.ForeignKey(UserScoreModifierType, models.CASCADE)
     date_time = models.DateTimeField(auto_now_add=True)
