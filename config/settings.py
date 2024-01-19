0680ca0b96179f851075fb965b8a43dc5305d02b
Alvaro Scelza <alvaroscelza@gmail.com>
2024-01-19 19:47:17 -0300

    rolled back settings changes

--- config/settings.py
+++ config/settings.py
@@ -2,153 +2,90 @@ import os
 from django.utils.translation import gettext_lazy as _
 from pathlib import Path
 
-# Meta
-APP_NAME = "Claims and Complaints"
-APP_DESCRIPTION = _(
-    "This project is a Django application that allows users to create claims and complaints about "
-    "different companies."
-)
-
-# Base Settings
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
+APP_NAME = 'Claims and Complaints'
+APP_DESCRIPTION = _('This project is a Django application that allows users to create claims and complaints about '
+                    'different companies.')
+AUTH_PASSWORD_VALIDATORS = [{'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'}]
 BASE_DIR = Path(__file__).parent.parent
-
-
-# Enviroment Variables
-IS_PRODUCTION = os.getenv("IS_PRODUCTION") != "False"
-SECRET_KEY = os.getenv("SECRET_KEY")
-allowed_hosts_env = os.getenv("ALLOWED_HOSTS")
-if IS_PRODUCTION and not allowed_hosts_env:
-    raise Exception("Allowed hosts must be defined in env")
-ALLOWED_HOSTS = (
-    allowed_hosts_env.split(",") if IS_PRODUCTION or allowed_hosts_env else ["*"]
-)
-
-# Configured Values
-DEBUG = os.getenv("DEBUG") == "True" or not IS_PRODUCTION
-
-
-# Django Base Configurations
-ROOT_URLCONF = "config.urls"
-AUTH_PASSWORD_VALIDATORS = [
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"}
-]
-
-
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.locale.LocaleMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
-REST_FRAMEWORK = {
-    "DATETIME_FORMAT": "%Y-%m-%d %H:%M:%S.%f",
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.TemplateHTMLRenderer"],
-    "EXCEPTION_HANDLER": "applications.utils.exception_handler",
-    "PAGE_SIZE": 10,
-}
-if IS_PRODUCTION and not os.getenv("DATABASE_NAME"):
-    raise Exception("Postgres Database not defined for production, please check .env")
-
-if not IS_PRODUCTION and not os.getenv("DATABASE_NAME"):
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": os.getenv("DATABASE_NAME"),
-            "USER": os.getenv("DATABASE_USER"),
-            "PASSWORD": os.getenv("DATABASE_PASSWORD"),
-            "HOST": os.getenv("DATABASE_HOST"),
-            "PORT": os.getenv("DATABASE_PORT"),
-        }
-    }
-# Django Security Configurations
 CORS_ALLOW_ALL_ORIGINS = True
-# CSRF_TRUSTED_ORIGINS = os.getos.getenv("CSRF_TRUSTED_ORIGINS", "http://localhost").split(",")
-
-
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://localhost').split(',')
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
+    }
+}
+DEBUG = os.getenv('DEBUG', False)
 # region INSTALLED_APPS
 DJANGO_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles'
 ]
-LOCAL_APPS = ["applications.apps.UsersConfig", "applications.apps.CoreConfig"]
-EXTERNAL_APPS = [
-    "rest_framework",
-    "crispy_forms",
-    "crispy_bootstrap5",
-]
-INSTALLED_APPS = DJANGO_APPS + EXTERNAL_APPS + LOCAL_APPS
+LOCAL_APPS = ['applications.apps.CoreConfig']
+EXTERNAL_APPS = ['rest_framework']
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + EXTERNAL_APPS
 # endregion INSTALLED_APPS
-
 # region Internationalization
-LANGUAGE_CODE = os.getenv("LANGUAGE_CODE")
-LOCALE_PATHS = [os.path.join(BASE_DIR, "locale")]
-LANGUAGES = [("es", _("Spanish")), ("en-us", _("English (US)"))]
-TIME_ZONE = "America/Montevideo"
+LANGUAGE_CODE = 'es'
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
+LANGUAGES = [('es', _('Spanish'))]
+TIME_ZONE = 'America/Montevideo'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 # endregion
-
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+REST_FRAMEWORK = {
+    'DATETIME_FORMAT': '%Y-%m-%d %H:%M:%S.%f',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_RENDERER_CLASSES': ['rest_framework.renderers.TemplateHTMLRenderer'],
+    'EXCEPTION_HANDLER': 'applications.utils.exception_handler',
+    'PAGE_SIZE': 10
+}
+ROOT_URLCONF = 'config.urls'
+SECRET_KEY = os.getenv('SECRET_KEY')
 # region Static files (CSS, JavaScript, Images)
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "static_storage"
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_storage')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # endregion
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [TEMPLATE_DIR],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
-            "libraries": {
-                "generic_helper": "templatetags.custom_tags",
+            'libraries': {
+                'generic_helper': 'templatetags.custom_tags',
             },
         },
     },
 ]
-WSGI_APPLICATION = "config.wsgi.application"
-
-# Auth
-
-AUTH_USER_MODEL = "users.User"  # Set default user model
-LOGIN_URL = "/accounts/login/"
-LOGIN_REDIRECT_URL = "/"
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-CRISPY_TEMPLATE_PACK = "bootstrap5"
-
-# Email Variables
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = os.getenv("EMAIL_USERNAME")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
-EMAIL_FROM_NAME = "Claims & Complaints"  # The name users will see in their inbox
-
-# Branding
-BRAND_NAME = "Claims & Complaints"
-SITE_URL = "http://127.0.0.1"
+WSGI_APPLICATION = 'config.wsgi.application'
