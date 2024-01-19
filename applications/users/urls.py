16b69fe7cf1dbe8be3f206a57d2a6c988acc1701
Alvaro Scelza <alvaroscelza@gmail.com>
2024-01-19 19:38:06 -0300

    refactor urls

--- applications/users/urls.py
+++ applications/users/urls.py
@@ -1,20 +1,15 @@
 from django.urls import path
 from . import views
 
-app_name = "users"
+app_name = 'users'
 
 urlpatterns = [
-    path("profile/", views.profile, name="profile"),
-    path("signup/", views.register, name="register"),
-    path("login/resend/<int:resend_user_id>/", views.login, name="login_resend"),
-    path("login/", views.login, name="login"),
-    path("logout/", views.logout, name="logout"),
-    path(
-        "forgot-password/<int:user_id>/<token>/",
-        views.forgot_password,
-        name="forgot_password",
-    ),
-    path("forgot-password/", views.forgot_password, name="forgot_password"),
-    path("verify/<int:user_id>/<token>", views.verify_email, name="verify_email"),
+    path('profile/', views.profile, name='profile'),
+    path('signup/', views.register, name='register'),
+    path('login/resend/<int:resend_user_id>/', views.login, name='login_resend'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('forgot-password/<int:user_id>/<token>/', views.forgot_password, name='forgot_password'),
+    path('forgot-password/', views.forgot_password, name='forgot_password'),
+    path('verify/<int:user_id>/<token>', views.verify_email, name='verify_email'),
 ]
-# login, register, profile,logout
