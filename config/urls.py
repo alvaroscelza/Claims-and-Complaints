ecb9c980f4ca4ad74600a3a16a07a9549c800e9d
Alvaro Scelza <alvaroscelza@gmail.com>
2024-01-19 19:51:36 -0300

    rolled back urls changes

--- config/urls.py
+++ config/urls.py
@@ -3,30 +3,32 @@ from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
 
 # region Admin configuration
-urlpatterns = i18n_patterns(
-    path("admin/", admin.site.urls), prefix_default_language=False
-)
-admin.site.site_header = admin.site.site_title = settings.APP_NAME
+urlpatterns = i18n_patterns(path('admin/', admin.site.urls), prefix_default_language=False)
+admin.site.site_header = settings.APP_NAME
 admin.site.index_title = settings.APP_DESCRIPTION
+admin.site.site_title = settings.APP_NAME
 # endregion
 
-# # region Core configuration
-# core_path = path('api/v1/core/', include('applications.core.urls'))
-# urlpatterns += i18n_patterns(core_path, prefix_default_language=False)
-# # endregion
+# region Core configuration
+core_path = path('', include('applications.core.urls'))
+urlpatterns += i18n_patterns(core_path, prefix_default_language=False)
+# endregion
+
+# region Users configuration
+users_path = path('users/', include('applications.users.urls'))
+urlpatterns += i18n_patterns(users_path, prefix_default_language=False)
+# endregion
 
 # region Media configuration
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # endregion
 
-# region Templates configuration
-urlpatterns += i18n_patterns(
-    path("", TemplateView.as_view(template_name="index.html"), name="home"),
-    path("accounts/", include("applications.users.urls", namespace="users")),
-    prefix_default_language=False,
-)
+# region Generic Templates configuration
+# contact_template = TemplateView.as_view(template_name='contact.html')
+# urlpatterns += i18n_patterns(path(r'contact/', contact_template), prefix_default_language=False)
+# terms_and_conditions_template = TemplateView.as_view(template_name='terms_and_conditions.html')
+# urlpatterns += i18n_patterns(path(r'terms/', terms_and_conditions_template), prefix_default_language=False)
 # endregion
