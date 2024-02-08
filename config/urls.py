50fac5ceb248664a477f9c98faf7621d3722cc80
Federico Silvera <federiconicolas007@gmail.com>
2024-02-08 17:28:00 -0300

    b

--- config/urls.py
+++ config/urls.py
@@ -28,7 +28,6 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # endregion
 
-
 # region Generic Templates configuration
 contact_template = path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact')
 urlpatterns += i18n_patterns(contact_template, prefix_default_language=False)
